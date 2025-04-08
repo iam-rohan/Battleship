0e885818572f5a28dcb30eb201ab1538eea0beaa
Rohan Acharya <rohanacharya42@gmail.com>
2025-04-08 20:50:02 +0545

    feat: add required logic for player creation with type

---
+++ src/player.js
@@ -0,0 +1,12 @@
+import { Gameboard } from "./gameBoard";
+
+export class Player {
+  constructor(type) {
+    this.type = type;
+    this.board = new Gameboard();
+  }
+
+  attackOn(x, y) {
+    this.board.receiveAttack(x, y);
+  }
+}
